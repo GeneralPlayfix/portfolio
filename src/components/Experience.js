28e0c431c345f00162fa9170f3308ba6b71ec667
Florian Fournier <florian.fournier1@epsi.fr>
2022-05-02 20:43:32 +0200

    Ajout du component experience
    Le component experience regroupe mes expériences professionnelles, personnelles et celles de ma scolarité. Il est responsive mais il manque une partie des expériences.

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ExperienceCard from './ExperienceCard';
 
 const Experience = () => {
+    const [data, setData] = useState(require("../data/experiences.json"))
+    const [projectType, setProjectType] = useState("1")
+    // useEffect(() => {
+    //     setData(require("../data/experiences.json"))
+
+    // }, [])
     return (
         <section id="experience">
             <div className="container">
                 <div className="buttons">
-                    <a>Projet Entreprise</a>
-                    <a>Projet EPSI</a>
-                    <a>Projet Solo</a>
+                    <a id="1" className={projectType === "1" ? "active" : ""} onClick={(e) => setProjectType(e.target.id)}>Projet Entreprise</a>
+                    <a id="2" className={projectType === "2" ? "active" : ""} onClick={(e) => setProjectType(e.target.id)}>Projet EPSI</a>
+                    <a id="3" className={projectType === "3" ? "active" : ""} onClick={(e) => setProjectType(e.target.id)}>Projet Solo</a>
                 </div>
-                <div className="content">
-                    p
+                <div className="content-2">
+                    {
+                        data.filter((element) => element.projectType === projectType).map((experience) =>
+                            <ExperienceCard key={experience.id} experience={experience} />
+                        )
+                    }
+                    {/* {
+                        data.filter((element) => element.projectType == "2").map((experience) =>
+                            <ExperienceCard key={experience.id} experience={experience} />
+                        )
+                    } */}
                 </div>
             </div>
         </section>
---
+++ src/components/ExperienceCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import cascade from '../img/cascade.jpg'
+const ExperienceCard = (props) => {
+    const { experience } = props
+    return (
+        <div className='ExperienceCard'>
+            <h2>{experience.name}</h2>
+            {/* <img src={process.env.PUBLIC_URL + "/img/experience/" + experience.id + "/cascade.jpg"} alt="" /> */}
+            <h3 className='company'>{experience.company}</h3>
+            <p className='description'>{experience.description}</p>
+            <p className='languages'>Languages : {experience.languages.split("|").join(', ')} </p>
+
+            <div className="buttons-2">
+                <button>Galerie</button>
+                {
+                    experience.sourceCodeLink === "" ? "" : <a href={experience.sourceCodeLink} target="_blank">Code source</a>
+                }
+                {
+                    experience.websiteLink === "" ? "" : <a href={experience.websiteLink} target="_blank">Lien site</a>
+
+                }
+            </div>
+
+
+        </div>
+    );
+};
+
+export default ExperienceCard;
